579774f6b14f9baa66d0783afb27ab1780aa3fde
Iury Assunção <iuryifba@gmail.com>
2025-05-12 23:12:24 -0300

    feat: add prisma and swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('ADEC API')
+    .setDescription('Endpoint da api da adec')
+    .setVersion('1.0')
+    .addTag('members')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
